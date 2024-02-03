67f4f1fbb0fdb999f2e7244d1206b511c1a011be
Ritesh Bucha <bucharitesh@gmail.com>
2024-02-04 00:31:10 +0530

    chore: minor fix

--- app/(protected)/avatars/page-client.tsx
+++ app/(protected)/avatars/page-client.tsx
@@ -5,20 +5,12 @@ import CreateAvatarButton from "@/components/ui/avatars/create-avatar-button";
 import Content from "@/components/ui/dashboard/Content";
 import NoAvatars from "@/components/ui/home/No-avatars";
 import { getAvatars } from "@/lib/api/http";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import React, { useEffect, useState } from "react";
 
 const AvatarSelectClient = () => {
   const [avatars, setAvatars] = useState<any[]>([]);
-  const [show, setShow] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShow(false);
-    }, 5000);
-  }, []);
 
   useEffect(() => {
     const api = async () => {
@@ -34,7 +26,10 @@ const AvatarSelectClient = () => {
     <div className="z-20 flex h-screen flex-col items-start gap-3 py-4 overflow-auto croll w-full">
       <div className="fixed bg-gradient-to-t from-transparent via-transparent to-black/80 h-16 w-full top-0 left-0 z-40"></div>
       <div className="flex gap-4 font-extrabold text-xl text-white mt-8 px-6 items-center">
-        <Icon icon={"back"} size={24} onClick={() => router.push('/home')} /> My Avatars
+        <Link href="">
+          <Icon icon="back" size={24} />
+        </Link>
+        My Avatars
       </div>
       {avatars.length !== 0 && <Content avatars={avatars} />}
       {avatars.length === 0 && <NoAvatars />}
