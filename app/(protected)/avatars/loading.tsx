add763fbc65aadf62ee46b6a3431859d3da14701
Pranav <prnvshanmukh00@gmail.com>
2024-02-04 13:53:12 +0530

    chore: loading state avatar and videos

---
+++ app/(protected)/avatars/loading.tsx
@@ -0,0 +1,13 @@
+import Spinner from "@/components/Spinner";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="text-white/50 flex w-full h-full items-center justify-center text-2xl z-50 font-bold gap-2">
+      <Spinner spinnerClassName="text-primary" />
+      Loading avatars...
+    </div>
+  );
+};
+
+export default Loading;
